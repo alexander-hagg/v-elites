450fdfa2fcfed62f7ad6724fd0d92f225f60554c
alexander-hagg <alexander.hagg@h-brs.de>
2019-07-26 13:48:41 +0200

    Fixed workaround (check children of UIFigure)

--- domain/npoly/showExample.m
+++ domain/npoly/showExample.m
@@ -3,7 +3,7 @@ function showExample(figHandle,d,varargin)
 
 genome = randn(2*d.dof,1); if nargin>2; genome = varargin{1}; end
 
-title(figHandle,[int2str(d.dof) '-Polygon']);
+%title(figHandle,[int2str(d.dof) '-Polygon']);
 pgon = polyshape(genome(1:2:end),genome(2:2:end));
 pgon.Vertices(:,1) = pgon.Vertices(:,1);
 pgon.Vertices(:,2) = pgon.Vertices(:,2);
