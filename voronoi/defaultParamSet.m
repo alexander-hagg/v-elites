8f9ecca626400bc188601bec1db67cdc0dfb2765
Alexander Hagg <info@haggdesign.de>
2019-08-05 16:41:03 +0200

    Update

--- voronoi/defaultParamSet.m
+++ voronoi/defaultParamSet.m
@@ -20,7 +20,7 @@ p.nGens                     = 25;
 p.numInitSamples            = 2^5;
 p.selectProcedure           = 'random'; % random bin
 p.maxBins                   = 2^3; % not used yet
-p.competeDistance           = 0.04;
+p.competeDistance           = 0.03;
 p.infReplacement            = 5;
 
 % Visualization and data management
